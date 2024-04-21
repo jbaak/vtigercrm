69729ee03679ddbbf5efe42bf10fe6e3ad0f41ce
Jesus Baak <jesus.baak@growproexperience.com>
2024-04-20 18:42:56 -0600

    Se corrige mensaje al agregar contatco desde web

--- newcontact/js/index.js
+++ newcontact/js/index.js
@@ -2,11 +2,14 @@
 	$('#contactForm').submit(function(event){
 
 		event.preventDefault();
+		$('#alert-success').addClass('d-none');
+		$('#alert-error').addClass('d-none');
 
 		$.ajax({
 			type: 'POST',
 			url: $(this).attr('action'),
 			data: $(this).serialize(),
+			dataType: 'json',
 			success: function(response) {
 				if (response.status == 'success') {
 					$('#alert-success').removeClass('d-none');
